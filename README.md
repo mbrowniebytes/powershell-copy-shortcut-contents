33f74d80c4a39a139f96c83442ac18ea4f3ae3c0
mbrowniebytes <mbrowniebytes@gmail.com>
2022-10-30 18:34:13 -0500

    fix subdirs not being created on first run updated based on PR #4 from l-palafox, thanks! also added second progress bar for shortcut being processed showing number of files being copied show total number of files copied and time taken moved options from vars to an opts object, so opts usage clearer in script more validation of source and destination opts script formatting, tabs to spaces added testdirs/ for testing

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Powershell script to copy files based on shortcuts
 
 From the source directory, find all shortcuts, and copy the shortcut contents to a destination directory
 
-Installation
+Run
 ------------
 
 Right click on _copy_shortcut_contents.ps1 and choose Run with Powershell
@@ -14,30 +14,32 @@ Usage
 Open _copy_shortcut_contents.ps1 and edit the options
 ```
 # options 
+$opts = @{
+    # only output what would happen, dont actually copy anything
+    dryrun = 1
 
-# only output what would happen, dont actually copy anything
-$dryrun = 1;
+    # where to copy from; full path; end with slash
+    # use current dir as srcDir
+    # srcDir = $scriptPath + "\"
+    # srcDir = "C:\Temp\"
+    srcDir = $scriptPath + "\testdirs\"
 
-# where to copy from; full path; end with slash
-# $src = "C:\Temp\"
-# use current dir as srcDir
-$srcDir = (Get-Item -Path ".\" -Verbose).FullName + "\"
+    # where to copy to; full path; end with slash
+    # destDir = "T:\"
+    destDir = $scriptPath + "\testdirs\testdest\"
 
-# where to copy to; full path; end with slash
-# $destDir = "T:\"
-$destDir = "T:\"
+    # 1 (recommended) - use dos copy to get progress per file; 0 - use copy-item
+    useCopyProgress = 1
 
-# 1 (recommended) - use dos copy to get progress per file, 0 - use copy-item 
-$useCopyProgress = 1;
+    # 1 - if a sub dest path exists, skip; 0 (recommended) - re-check files, maybe new, in sub dest paths
+    skipExistingDestPath = 0
 
-# if dest path exists, skip
-$skipExistingDestPath = 0;
+    # 1 (recommended) - if dest file exists, skip; 0 - re-copy
+    skipExistingDestFile = 1
 
-# 1 (recommended) - if dest file exists, skip, 0 - re-copy
-$skipExistingDestFile = 1;
-
-# 1 - show existing files, 0 - dont show, reducing output
-$showExistingFiles = 0;
+    # 1 - show existing files; 0 (recommended) - don't show, reducing output
+    showExistingFiles = 0
+}
 ```
 
 Example usage
@@ -52,3 +54,32 @@ Example usage
 
 ![05 copied files.png](screenshots/05%20copied%20files.png)
 
+### Known issues
+* If you run the script in Windows Terminal, the history of the write-host output will be lost to the Write-Progress.  Workaround: run the script in native PowerShell, or update Windows Terminal -> Settings -> Default terminal application -> to Window Console Host eg not Windows Terminal  
+* shortcuts are only processed from one dir, not recursively; However, the shortcuts target folder contents are recursively copied 
+* running local PowerShell scripts requires updating your PowerShell security policy
+  * Run PowerShell as Admin
+  * Set-ExecutionPolicy RemoteSigned  
+
+    The execution policies you can use are:  
+    * **Restricted** - Scripts won’t run.  
+    * **RemoteSigned** - Scripts created locally will run, but those downloaded from the Internet will not (unless they are digitally signed by a trusted publisher).  
+    * **AllSigned** - Scripts will run only if they have been signed by a trusted publisher.  
+    * **Unrestricted** - Scripts will run regardless of where they have come from and whether they are signed.  
+      [stackoverflow](https://security.stackexchange.com/questions/1801/how-is-powershells-remotesigned-execution-policy-different-from-allsigned)
+
+## Change Log
+### 2022-10-30 tag 0.1.0
+* fix subdirs not being created on first run  
+updated based on [PR #4](https://github.com/mbrowniebytes/powershell-copy-shortcut-contents/pull/4) from [l-palafox](https://github.com/l-palafox), thanks!  
+
+also added  
+* second progress bar for shortcut being processed showing number of files being copied  
+* show total number of files copied and time taken
+* moved options from vars to an opts object, so opts usage clearer in script
+* more validation of source and destination opts  
+* script formatting, tabs to spaces
+* added testdirs/ for testing
+
+### 2018-06-23 tag 0.0.1
+initial
--- _copy_shortcut_contents.ps1
+++ _copy_shortcut_contents.ps1
@@ -2,199 +2,338 @@
 #
 # from the source directory, find all shortcuts, and copy the shortcut contents to a destination directory
 
+# helper vars
+if ($psISE)
+{
+    $scriptPath = Split-Path -Parent -Path $psISE.CurrentFile.FullPath
+}
+else
+{
+    $scriptPath = $PSScriptRoot
+}
+$timeStartScript = Get-Date
 
-# options 
+# options
+$opts = @{
+    # only output what would happen, don't actually copy anything
+    dryrun = 1
 
-# only output what would happen, dont actually copy anything
-$dryrun = 1;
+    # where to copy from; full path; end with slash
+    # run script in dir with shortcuts; use current dir as srcDir
+    # srcDir = $scriptPath + "\"
+    # srcDir = "C:\Temp\"
+    srcDir = $scriptPath + "\testdirs\"
 
-# where to copy from; full path; end with slash
-# $src = "C:\Temp\"
-# use current dir as srcDir
-$srcDir = (Get-Item -Path ".\" -Verbose).FullName + "\"
+    # where to copy to; full path; end with slash
+    # destDir = "T:\"
+    destDir = $scriptPath + "\testdirs\testdest\"
 
-# where to copy to; full path; end with slash
-# $destDir = "T:\"
-$destDir = "T:\"
+    # 1 (recommended) - use dos copy to get progress per file; 0 - use copy-item
+    useCopyProgress = 1
 
-# 1 (recommended) - use dos copy to get progress per file, 0 - use copy-item 
-$useCopyProgress = 1;
+    # 1 - if a sub dest path exists, skip; 0 (recommended) - re-check files, maybe new, in sub dest paths
+    skipExistingDestPath = 0
 
-# if dest path exists, skip
-$skipExistingDestPath = 0;
+    # 1 (recommended) - if dest file exists, skip; 0 - re-copy
+    skipExistingDestFile = 1
 
-# 1 (recommended) - if dest file exists, skip, 0 - re-copy
-$skipExistingDestFile = 1;
-
-# 1 - show existing files, 0 - dont show, reducing output
-$showExistingFiles = 0;
-
-
-# start
-
-# initial padding for progress bar
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-write-host ""
-
-Write-Progress -Id 1 -Activity "Copying shortcut contents from $srcDir to $destDir" -Status "Progress 0%" -PercentComplete 0
-write-host "Copying shortcut contents from $srcDir to $destDir"
-write-host ""
-
-if ($destDir -eq "") {
-	write-host "Stopped"
-	write-host "Edit srcDir and destDir variables and read script"
-	write-host "Press any key to continue..."
-	[void][System.Console]::ReadKey($true)
-	exit
+    # 1 - show existing files; 0 (recommended) - don't show, reducing output
+    showExistingFiles = 0
 }
 
+# functions
+function PauseAndExit()
+{
+    try
+    {
+        # write-host "debug: "$host.Name
+        if ($host.Name -eq "Windows PowerShell ISE Host")
+        {
+            $result = Read-Host "write-host "Press enter to continue...""
+        }
+        else
+        {
+            write-host "Press any key to continue..."
+            [void][System.Console]::ReadKey($true)
+        }
+    }
+    catch
+    {
+        # ide w/ redirect output
+        write-host "exiting..."
+    }
+    exit
+}
+
+# validate options
+if ($opts.destDir -eq "")
+{
+    write-host "Stopped"
+    write-host "Edit srcDir and destDir variables and read script"
+    PauseAndExit
+}
+if ($opts.destDir.substring($opts.destDir.length - 1, 1) -ne "\")
+{
+    write-host "Stopped "
+    write-host "Edit srcDir and destDir variables to end with / and read script"
+    PauseAndExit
+}
+if ($opts.srcDir -eq "")
+{
+    write-host "Stopped"
+    write-host "Edit srcDir and destDir variables and read script"
+    PauseAndExit
+}
+if ($opts.srcDir.substring($opts.srcDir.length - 1, 1) -ne "\")
+{
+    write-host "Stopped"
+    write-host "Edit srcDir and destDir variables to end with / and read script"
+    PauseAndExit
+}
+
+#
+# start
+#
+
+# padding for progress
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+write-host ""
+
+Write-Progress -Id 0 -Activity "Copying shortcut contents in $( $opts.srcDir )" -Status "Progress 0%" -PercentComplete 0
+write-host "Copying shortcut contents from $( $opts.srcDir ) to $( $opts.destDir )"
+write-host ""
+
+# write-host "debug: srcDir:" $opts.srcDir
+# write-host "debug: destDir:" $opts.destDir
+# PauseAndExit
+
 # Get all shortcuts
-$shortcuts = gci "$srcDir\*.lnk"
+$shortcuts = gci "$( $opts.srcDir )\*.lnk"
 
 # iterate over shortcuts
 $total = $shortcuts.count
+$totalFilesCopied = 0
 $index = 0
+$progress = 0
 $sh = New-Object -COM WScript.Shell
-foreach ($shortcut in $shortcuts) {
-	# get target path from shortcut ie what shortcut points to	
-	$srcPath = $sh.CreateShortcut($shortcut.fullname).Targetpath
-	$srcDrive = $srcPath.substring(0, 3)
-	
-	# duplicate src folder structure in dest
-	$destPath = $srcPath.replace($srcDrive, $destDir)
+foreach ($shortcut in $shortcuts)
+{
+    # get target path from shortcut ie what shortcut points to
+    $srcPath = $sh.CreateShortcut($shortcut.fullname).Targetpath
+    $srcRelPath = $srcPath.replace($opts.srcDir, "")
+    $srcDrive = $srcPath.substring(0, 3)
 
-	# indicate progress	
-	$progress = "{0:N2}" -f ($index / $total * 100)
-	$index++
-	# -NoNewLine if not using xcopy
-	write-host " Copying: $srcPath -> $destPath " -foregroundcolor DarkGreen -backgroundcolor white
-	Write-Progress -Id 1 -Activity "Processing $index of $total shortcuts" -Status "Progress $progress%" -PercentComplete $progress	
-	
-	# skip existing dirs
-	if (Test-Path "$destPath") {
-		write-host " $destPath " -NoNewLine
-		write-host " Exists "  -foregroundcolor white -backgroundcolor DarkGreen	
-		if ($skipExistingDestPath) {
-			continue
-		}
-	}
-	
-	# get dest path for copy-item to recurse into	
-	if (Test-Path "$destPath") {
-		# if path exists, copy-item will re-create within it, so go up one level
-		$destRecreatePath = "$destPath\..\"	
-	} else {
-		# if path does not exist, copy-item will create it
-		$destRecreatePath = $destPath
-	}	
-	
-	# copy shortcut contents to destination, including subdirs
-	if (!$useCopyProgress) {
-		# use copy-item to copy shortcut contents to destination, including subdirs
-		# but no progress per file
-		if ($dryrun) {
-			write-host "dryrun: copy-item -Path $srcPath -Destination $destRecreatePath -Force -Recurse -Container -Exclude $exclude"
-		} else {
-			# get current list of files, if any
-			$exclude = ""
-			if (Test-Path "$destPath") {
-				$origFiles = gci "$destPath" -File -Recurse
-				
-				# exclude already copied dirs/files
-				if ($skipExistingDestFile) {
-					$exclude = gci $destPath
-				}				
-			} else {
-				$origFiles = @()
-			}
-			
-			# copy
-			copy-item -Path "$srcPath" -Destination "$destRecreatePath" -Force -Recurse -Container -Exclude $exclude
-			
-			# show results in destPath, from current or prior copy-item
-			$files = gci "$destPath" -File -Recurse
-			foreach ($file in $files) {			
-				$srcFile = $file.fullname
-				$destFile = $srcFile.replace($srcDrive, $destDir)	
-				$destRelPath = $destFile.replace($destPath, "")								
-				if ($origFiles.fullname -contains $file.fullname) {
-					if ($showExistingFiles) {
-						write-host " $destRelPath " -NoNewLine
-						write-host " Exists "
-					}
-				} else {
-					write-host " $destRelPath " -NoNewLine
-					write-host " Copied "  -foregroundcolor white -backgroundcolor DarkGreen
-				}
-			}			
-		}
-		
-	} else {			
-		# use msdos copy to copy shortcut contents to destination, including subdirs
-		
-		# copy folder structure only ie create srcPath directories in destRecreatePath
-		if ($dryrun) {
-			write-host "dryrun: copy-item -Path $srcPath -Destination $destRecreatePath -Filter {PSIsContainer} -Force -Recurse -Container"
-		} else {		
-			# https://stackoverflow.com/questions/9996649/copy-folders-without-files-files-without-folders-or-everything-using-powershel
-			copy-item -Path "$srcPath" -Destination "$destRecreatePath" -Filter {PSIsContainer} -Force -Recurse -Container					
-		}
-		
-		# copy files with progress
-		$files = gci "$srcPath" -File -Recurse
-		foreach ($file in $files) {
-			$srcFile = $file.fullname
-			$destFile = $srcFile.replace($srcDrive, $destDir)
-			
-			# skip existing files, compare by date
-			if (Test-Path "$destFile") {
-				$srcLastTime = $file.LastWriteTime				
-				$destLastTime = (gci "$destFile").LastWriteTime
-				if ($srcLastTime -eq $destLastTime) {
-					if ($showExistingFiles) {
-						write-host " $destFile " -NoNewLine
-						write-host " Exists "	
-					}						
-					if ($skipExistingDestFile) {
-						continue
-					}
-				}
-			}		
-			
-			# show file name being copied
-			$destRelPath = $destFile.replace($destPath, "")
-			# spacing for copy output 			
-			#          '100% copied         1 file(s) copied. '
-			write-host "                                      $destRelPath" -NoNewLine
-			# copy with progress %
-			if ($dryrun) {
-				write-host ""
-				write-host "dryrun: cmd /c copy /z $srcFile $destFile"
-			} else {
-				# https://stackoverflow.com/questions/2434133/progress-during-large-file-copy-copy-item-write-progress
-				# xcopy prompts for is this a file/dir, no progress
-				# robocopy asks for admin perms on ntfs/audit attribs
-				# copy copies with progress %
-				# /z   : Copies networked files in restartable mode. 				
-				cmd /c copy /z $srcFile $destFile
-			}
-		}		
-	}
-	# copied, update progress
-	$progress = "{0:N2}" -f ($index / $total * 100)
-	Write-Progress -Id 1 -Activity "Processing $index of $total shortcuts" -Status "Progress $progress%" -PercentComplete $progress
+    # duplicate src folder structure in dest
+    $destPath = $srcPath.replace($srcDrive, $opts.destDir)
+    $destRelPath = $destPath.replace($opts.destDir, "")
+
+    # write-host "debug: srcPath: $srcPath"
+    # write-host "debug: destPath: $destPath"
+
+    # shortcut processing progress
+    $progress = "{0:N2}" -f ($index / $total * 100)
+    $index++
+    Write-Progress -Id 0 -Activity "Processing $index of $total shortcuts in $srcRelPath" -Status "Progress $progress%" -PercentComplete $progress
+    write-host " Copying: $srcPath -> $destPath " -foregroundcolor DarkGreen -backgroundcolor white
+
+    # skip existing dirs
+    if (Test-Path "$destPath")
+    {
+        write-host " $destPath " -NoNewLine
+        write-host " Exists "  -foregroundcolor white -backgroundcolor DarkGreen
+        if ($opts.skipExistingDestPath)
+        {
+            continue
+        }
+    }
+
+    # copy shortcut contents to destination, including subdirs
+    if (!$opts.useCopyProgress)
+    {
+
+        # get current list of files, if any
+        $exclude = ""
+        if (Test-Path "$destPath")
+        {
+            $origFiles = gci "$destPath" -File -Recurse
+
+            # exclude already copied dirs/files
+            if ($skipExistingDestFile)
+            {
+                $exclude = gci $destPath
+            }
+        }
+        else
+        {
+            $origFiles = @()
+        }
+
+        # use copy-item to copy shortcut contents to destination, including subdirs
+        # but no progress per file
+        if ($opts.dryrun)
+        {
+            write-host "dryrun: copy-item -Path $srcPath -Destination $destRecreatePath -Force -Recurse -Container -Exclude $exclude"
+        }
+        else
+        {
+            # get dest path for copy-item to recurse into
+            if (Test-Path "$destPath")
+            {
+                # if path exists, copy-item will re-create within it, so go up one level
+                $destRecreatePath = "$destPath\..\"
+            }
+            else
+            {
+                # if path does not exist, copy-item will create it
+                $destRecreatePath = $destPath
+            }
+            # write-host "debug: destRecreatePath: $destRecreatePath"
+
+            # copy items
+            copy-item -Path "$srcPath" -Destination "$destRecreatePath" -Force -Recurse -Container -Exclude $exclude
+
+            # show results in destPath, from current or prior copy-item
+            $files = gci "$destPath" -File -Recurse
+            foreach ($file in $files)
+            {
+                $destFile = $file.fullname
+                $destRelPath = $destFile.replace($destPath, "")
+
+                if ($origFiles.fullname -contains $file.fullname)
+                {
+                    if ($opts.showExistingFiles)
+                    {
+                        write-host " $destRelPath " -NoNewLine
+                        write-host " Exists "
+                    }
+                }
+                else
+                {
+                    write-host " $destRelPath " -NoNewLine
+                    write-host " Copied "  -foregroundcolor white -backgroundcolor DarkGreen
+                    $totalFilesCopied++
+                }
+            }
+        }
+
+    }
+    else
+    {
+        # use msdos copy to copy shortcut contents to destination, including subdirs
+
+        # get files to copy
+        $files = gci "$srcPath" -File -Recurse
+
+        # file copy progress
+        $totalFiles = $files.count
+        $indexFile = 0
+        $progressFile = 0
+        Write-Progress -Id 1 -Activity "   Copying shortcut contents in $srcRelPath" -Status "Progress 0%" -PercentComplete 0
+
+        # copy files
+        foreach ($file in $files)
+        {
+            $progress = "{0:N2}" -f ($indexFile / $totalFiles * 100)
+            $indexFile++
+
+            $srcFile = $file.fullname
+            $srcRelFile = $srcFile.replace($opts.srcDir, "")
+            $destFile = $srcFile.replace($srcDrive, $opts.destDir)
+            $destPath = Split-Path $destFile
+            $destRelPath = $destFile.replace($destPath, "")
+
+            # write-host "debug: srcPath: $srcPath"
+            # write-host "debug: destFile: $destFile"
+            # write-host "debug: destPath: $destPath"
+
+            Write-Progress -Id 1 -Activity "   Copying $indexFile of $totalFiles files .. $srcRelFile" -Status "Progress $progress%" -PercentComplete $progress
+
+            # create destPath folder structure
+            if (!(Test-Path "$destPath"))
+            {
+                if ($opts.dryrun)
+                {
+                    write-host "dryrun: New-Item -ItemType Directory -Force -Path $destPath"
+                }
+                else
+                {
+                    # https://stackoverflow.com/questions/16906170/create-directory-if-it-does-not-exist
+                    # write-host "created destPath $destPath"
+                    New-Item -ItemType Directory -Force -Path "$destPath" | Out-Null
+                }
+            }
+
+            # skip existing files, compare by date
+            if (Test-Path "$destFile")
+            {
+                $srcLastTime = $file.LastWriteTime
+                $destLastTime = (gci "$destFile").LastWriteTime
+                if ($srcLastTime -eq $destLastTime)
+                {
+                    if ($opts.showExistingFiles)
+                    {
+                        write-host " $destFile " -NoNewLine
+                        write-host " Exists "
+                    }
+                    if ($opts.skipExistingDestFile)
+                    {
+                        continue
+                    }
+                }
+            }
+
+            # show file name being copied
+            # spacing for copy output
+            #          '100% copied         1 file(s) copied. '
+            write-host "                                       $destRelPath" -NoNewLine
+
+            # write-host "debug: srcFile: $srcFile ->"
+            # write-host "debug: destFile: $destFile"
+            # Start-Sleep -Milliseconds 1234
+
+            # copy with progress %
+            if ($opts.dryrun)
+            {
+                write-host ""
+                write-host "dryrun: cmd /c copy /z $srcFile $destFile"
+            }
+            else
+            {
+                # https://stackoverflow.com/questions/2434133/progress-during-large-file-copy-copy-item-write-progress
+                # xcopy prompts for is this a file/dir, no progress
+                # robocopy asks for admin perms on ntfs/audit attribs
+                # copy copies with progress %
+                # /z   : Copies networked files in restartable mode.
+                cmd /c copy /z $srcFile $destFile
+            }
+            $totalFilesCopied++
+        }
+
+        Write-Progress -Id 1 -Activity "   Copied shortcut contents in $srcRelPath" -Status "Progress 100%" -PercentComplete 100
+    }
 }
 
 # finish
-Write-Progress -Id 1 -Activity "Processed $index of $total shortcuts" -Status "Progress $progress%" -PercentComplete $progress
+Write-Progress -Id 0 -Activity "Copied $index of $total shortcut contents in $( $opts.srcDir )" -Status "Progress 100%" -PercentComplete 100
+Write-Progress -Id 1 -Completed $true
+
+$timeFinishScript = Get-Date
+$timeElapsedScript = $timeFinishScript - $timeStartScript
+
+write-host ""
+$runtime = $timeElapsedScript.ToString("hh\:mm\:ss")
+if ($opts.dryrun)
+{
+    write-host "dryrun: " -NoNewLine
+}
+write-host "Copied $totalFilesCopied files in $runtime"
+
 write-host ""
 write-host "Finished"
-write-host "Press any key to continue..."
-[void][System.Console]::ReadKey($true)
+PauseAndExit
